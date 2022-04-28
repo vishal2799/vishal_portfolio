802ae4d213bc9913d7b2722d5d166235552de0d5
Vishal Sharma <sharma.vishal2799@gmail.com>
2022-04-28 11:35:19 +0530

    fixed navigationdots file

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavigationDots = ({active}) => {
   return (
     <div className='app__navigation'>
-        {['home', 'about', 'work', 'skills', ,'testimonials', 'contact'].map((item, index) => (
+        {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
                     <a href={`#${item}`}
                     style={active === item ? {backgroundColor: '#313BAC'} : {}}
                     className="app__navigation-dot"
